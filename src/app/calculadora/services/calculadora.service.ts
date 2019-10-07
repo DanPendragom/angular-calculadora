733697dba6c8e91e16e65ad221e752e42c69ece4
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-10-07 05:18:33 -0300

    Criando serviços da calculadora

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { CalculadoraModule } from '../calculadora.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
 
+  // Definindo operações com constantes
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
+  // Método>Parametros>TipoDeRetorno
+  calcular(num1: number, num2: number, op: string): number {
+    let resultado: number;
+
+    switch (op) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+    return 0;
+  }
   constructor() { }
 }
